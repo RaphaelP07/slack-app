64499af7328f6b47d79840f14fd97add389565c5
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-03-15 02:31:06 +0800

    removed useEffect on dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Header from './Header'
 import SideBar from './SideBar'
 import Chat from './Chat'
 import ErrorPage from './ErrorPage'
 
 const Dashboard = ({ loggedUser }) => {
-  // useEffect (() => {
-    
-  // }, [])
-
   return (
     loggedUser === '' ?
     <ErrorPage />
