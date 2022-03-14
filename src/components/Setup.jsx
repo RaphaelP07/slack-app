b2eb22a78dd1771edba6839d4c3fb1f589c66156
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-03-15 04:26:50 +0800

    fixed import error

--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -2,6 +2,7 @@ import { useState, useContext } from "react";
 import slack from "../slack-logo.png";
 import { GlobalContext } from "../context/GlobalState";
 import { v4 as uuidv4 } from "uuid";
+import { Link } from "react-router-dom";
 
 const Setup = ({ loggedUser }) => {
   const [firstName, setFirstName] = useState("");
