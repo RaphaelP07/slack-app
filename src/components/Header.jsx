bb6610c8f39c08adbb4eaae79580368d7c702286
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-03-18 17:06:02 +0800

    added clear states when signing out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import logo from '../images/slack-logo.png'
 import { Link } from "react-router-dom";
+import { GlobalContext } from '../context/GlobalState';
 
 const Header = () => {
+  const { clearStates } = useContext(GlobalContext)
+
+  const signOutClear = () => {
+    localStorage.clear()
+    clearStates()
+    console.log('clear')
+  }
   return (
     <header className='dashboard-header'>
       <div className="logo-container">
           <img src={logo} className="slack-logo" alt="slack-logo" onClick={() => window.location.reload()}/>
       </div>
       <Link to="/slack-app">
-        <button className='sign-out' onClick={() => localStorage.clear()}>Sign Out</button>
+        <button className='sign-out' onClick={signOutClear}>Sign Out</button>
       </Link>
     </header>
   )
